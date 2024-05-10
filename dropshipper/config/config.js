e8898777d8325fac410fe01a7a9b5f8ef8af9ebc
andresrestrepozapata21 <arz.950203@gmail.com>
2024-05-10 08:42:37 -0500

    actualizacion

--- dropshipper/config/config.js
+++ dropshipper/config/config.js
@@ -2,7 +2,7 @@
 (function () {
     // Define tus URLs aquí
     const config = {
-        production: "http://localhost:3000"
+        production: "https://paky.diegoarbelaez.com"
     };
 
     // Asigna el objeto de configuración a una variable global
